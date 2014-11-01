fbbbea9657375bc2f7318629043eeb6f5ce0258a
Adriel Carsete <adriel.carsete@gmail.com>
2014-10-31 22:49:21 -0200

    criacao classe Fatorial.java e TesteFatorial.java

---
+++ Fatorial.java
@@ -0,0 +1,12 @@
+public class Fatorial {
+	public int calcularFatorial(int n) throws IllegalArgumentException{
+		if(n < 0)
+			throw new IllegalArgumentException("Não existe fatorial para numeros negativos");
+		int resultado = 1;
+		
+		for(int = 2; i <= n; i++){
+			resultado*=i;
+		}
+		return resultado;
+	}
+}
